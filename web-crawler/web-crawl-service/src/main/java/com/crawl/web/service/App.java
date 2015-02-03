1099b7996fb29a8de7e036384c4d6d7d5cc37ade
chandu-atina <chandu.atina@gmail.com>
2015-02-03 16:25:22 +0530

    Project Skeleton Structure

---
+++ web-crawler/web-crawl-service/src/main/java/com/crawl/web/service/App.java
@@ -0,0 +1,13 @@
+package com.crawl.web.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
