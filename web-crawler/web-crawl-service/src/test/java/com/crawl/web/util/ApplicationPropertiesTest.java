0451d057953b3a9f2cbb3417db8d652b36fbb994
chandu-atina <chandu.atina@gmail.com>
2015-02-09 18:41:26 +0530

    JUnit test for ApplicationProperties

---
+++ web-crawler/web-crawl-service/src/test/java/com/crawl/web/util/ApplicationPropertiesTest.java
@@ -0,0 +1,34 @@
+package com.crawl.web.util;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.crawl.web.entry.Crawler;
+
+import junit.framework.TestCase;
+/**
+ * Test Class to test whether application properties
+ * are loaded successfully or not
+ *
+ */
+public class ApplicationPropertiesTest extends TestCase{
+	
+	private static final String CONFIG_PATH = "classpath:Application-config-crawler.xml";
+
+	/**
+	 * 
+	 * @throws Exception
+	 * Check if all properties are loaded by spring IOC.
+	 * Asserts if any of the property is null
+	 */
+	@Test
+	public void testGetURLList() throws Exception {
+		final ApplicationContext context = new ClassPathXmlApplicationContext(
+				CONFIG_PATH);
+		final ApplicationProperties appProp = context.getBean(ApplicationProperties.class);
+		boolean flag=appProp.toString().contains("null");
+		assertFalse("Application Properties are not loaded successfully",flag);
+	}
+}
