56dc052f8b203a541a6407c9c943c65260322ef7
chandu-atina <chandu.atina@gmail.com>
2015-02-10 13:43:56 +0530

    Test suite to run all the test cases

---
+++ web-crawler/web-crawl-service/src/test/java/com/crawl/web/testsuite/AllTests.java
@@ -0,0 +1,21 @@
+package com.crawl.web.testsuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.crawl.web.service.CrawlerServiceTest;
+import com.crawl.web.service.impl.WebCrawlerServiceImplTest;
+import com.crawl.web.util.ApplicationPropertiesTest;
+import com.crawl.web.util.URLFormatterTest;
+/**
+ * Test suite to run all the test cases in single go
+ * @author chandrasekhara
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ WebCrawlerServiceImplTest.class, CrawlerServiceTest.class,
+		ApplicationPropertiesTest.class, URLFormatterTest.class })
+public class AllTests {
+
+}
